d7d7d27a2d36b0481bb834b316c50a03fd16f460
Faizel Quabili <faizelq@udel.edu>
2023-10-04 10:38:42 -0400

    Progress check - convole and reduce done

--- PR-1/main.py
+++ PR-1/main.py
@@ -11,21 +11,20 @@ with open('config.yaml', 'r') as config_file:
 image_file_path = config['file_locations'][0]  # Assuming 'lena.png' is the first entry
 
 # Load the image in grayscale
-image = cv2.imread(image_file_path, cv2.IMREAD_GRAYSCALE)
+image = cv2.imread(image_file_path)
 
-def create_gaussian_kernel_3x3(sigma):
-    kernel = np.array([[1, 2, 1],
-                       [2, 4, 2],
-                       [1, 2, 1]], dtype=np.float32)
-    kernel = kernel / (16.0 * np.pi * sigma**2)  # Normalize by the sum of all elements
-    return kernel
+# Problem 1 - convolve
+sobel_y_kernel = np.array([[-1, 0, 1],
+                            [-2, 0, 2],
+                            [-1, 0, 1]])
+cv2.imwrite("./images/convolve-function-lena.png", faiCv.convolve(image, sobel_y_kernel))
+cv2.imwrite("./images/test-cv2-filter2D-lena.png", cv2.filter2D(src = image, ddepth = -1, kernel = sobel_y_kernel))
 
-# Example usage:
-sigma = 1.0  # Adjust the sigma value as needed
-gaussian_kernel = create_gaussian_kernel_3x3(sigma)
-
-convolved_image = faiCv.convolve(image, gaussian_kernel)
-rgb_image = cv2.cvtColor(convolved_image, cv2.COLOR_GRAY2RGB)
-cv2.imwrite("lena-gaussian-blur.png", rgb_image)
-
-
+# Problem 2 - reduce
+cv2.imwrite("./images/reduce-function-lena.png", faiCv.reduce(image))
+x = faiCv.reduce(image)
+# Testing
+blurred_image = cv2.GaussianBlur(image, (3, 3), 0)
+resized_image = cv2.resize(blurred_image, (image.shape[1] // 2, image.shape[0] // 2))
+cv2.imwrite("./images/test-cv2-downsample-lena.png", resized_image)
+print(np.array_equal(x, resized_image)) # returns True
--- PR-1/pr1Functions.py
+++ PR-1/pr1Functions.py
@@ -31,10 +31,16 @@ def convolve(I_input, H):
 
     # Create output image of same size (using np.empty instead of np.zeros to save computational time/power)
     I_output = np.empty(I_input.shape)
-
-    # Loop through rows, cols, and each color pixel channel
     
-
+    # Loop through rows, cols, and each color pixel channel
+    for i in range(rows):
+        for j in range(cols):
+            for c in range(channels):
+                # np.reshape(M, -1) will flatten our matrix to a 1D array
+                # padded_image[i : i + kernel_cols, j : j + kernel_rows, k], slicing our array. [0 : 3, 0 : 3] will go from rows 0 (inclusive) to 3 (exluded)
+                I_output[i][j][c] = np.reshape(H, -1) @ np.reshape(padded_image[i : i + kernel_cols, j : j + kernel_rows, c], -1)
+    
+    return I_output
 
 #################################################################
 
@@ -42,8 +48,30 @@ def convolve(I_input, H):
 # by half the width and height of the input. Remember to Gaussian filter the image before reducing it;
 # use separable 1D Gaussian kernels.
 
-def reduce(I):
-    pass
+def reduce(I_input):
+
+    # Checking if image is appropriate for manipulation
+    if I_input is None:
+        raise ValueError("Image does not exist")
+    elif I_input.ndim < 2:
+        raise ValueError("Image must be 2D matrix")
+    else:
+        log.info("Image is exists and is valid")
+
+    # Height, width, RGB channels (channels should be 3 - R, G, B)
+    rows, cols, channels = I_input.shape
+
+    # Apply Gaussian blurring to image before downsampling
+    I_blurred = cv2.GaussianBlur(I_input, (3, 3), 0)
+
+    # Resize the filtered image to half its width and height
+    I_downsample = cv2.resize(I_blurred, (rows // 2, cols // 2))
+
+    return I_downsample
+
+
+
+
 
 #################################################################
 
