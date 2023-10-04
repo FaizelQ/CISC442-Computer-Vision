1439705fb3cf51677f9bcc3372bc9f61afebd7f5
Faizel Quabili <faizelq@udel.edu>
2023-10-04 19:05:58 -0400

    progress check - laplacian pyramid done

--- PR-1/main.py
+++ PR-1/main.py
@@ -8,19 +8,20 @@ with open('config.yaml', 'r') as config_file:
     config = yaml.safe_load(config_file)
 
 # Extract the file location for the image you want to process
-lena_file_path = config['file_locations'][0]  # Assuming 'lena.png' is the first entry
+# Assuming 'lena.png' is the first entry
+lena_file_path = config['file_locations'][0]
 
 # Load the image in grayscale
 image = cv2.imread(lena_file_path)
 
 # Problem 1 - convolve
 sobel_y_kernel = np.array([[-1, 0, 1],
-                            [-2, 0, 2],
-                            [-1, 0, 1]])
+                           [-2, 0, 2],
+                           [-1, 0, 1]])
 sobel_image = faiCv.convolve(image, sobel_y_kernel)
 cv2.imwrite("./images/convolve-sobel-lena.png", sobel_image)
 # Testing
-test_sobel_image = cv2.filter2D(src = image, ddepth = -1, kernel = sobel_y_kernel)
+test_sobel_image = cv2.filter2D(src=image, ddepth=-1, kernel=sobel_y_kernel)
 cv2.imwrite("./images/test-convolve-sobel.png", test_sobel_image)
 
 # Problem 2 - reduce
@@ -28,7 +29,8 @@ image_downsample = faiCv.reduce(image)
 cv2.imwrite("./images/reduce-lena.png", image_downsample)
 # Testing
 test_blurred_image = cv2.GaussianBlur(image, (3, 3), 0)
-test_resized_image = cv2.resize(test_blurred_image, (image.shape[1] // 2, image.shape[0] // 2))
+test_resized_image = cv2.resize(
+    test_blurred_image, (image.shape[1] // 2, image.shape[0] // 2))
 cv2.imwrite("./images/test-reduce-lena.png", test_resized_image)
 
 # Problem 3 - expand
@@ -38,5 +40,16 @@ cv2.imwrite("./images/expand-function-lena.png", image_upsampled)
 test_upsampled = cv2.pyrUp(image_downsample)
 cv2.imwrite("./images/test-expand-lena.png", test_upsampled)
 
+# Problem 4 - Gaussian Pyramid
+n = 4  # Number of levels
+gaussian_pyramid = faiCv.gaussianPyramid(image, n)
+for i in range(n):
+    cv2.imwrite(
+        f"./images/gaussian-pyramind-level{i + 1}-lena.png", gaussian_pyramid[i])
 
-
+# Problem 5 - Laplacian Pyramid
+n = 4  # Number of levels
+laplacian_pyramid = faiCv.laplacianPyramid(image, n)
+for i in range(n):
+    cv2.imwrite(
+        f"./images/laplacian-pyramind-level{i + 1}-lena.png", laplacian_pyramid[i])
--- PR-1/pr1Functions.py
+++ PR-1/pr1Functions.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import os
 import logging as log
 
 #################################################################
@@ -27,19 +26,21 @@ def convolve(I_input, H):
 
     # Padding will be needed for convolution, ex. 3x3, padding_length = 1
     padding_length = kernel_rows // 2
-    padded_image = cv2.copyMakeBorder(I_input, padding_length, padding_length, padding_length, padding_length, cv2.BORDER_CONSTANT)
+    padded_image = cv2.copyMakeBorder(
+        I_input, padding_length, padding_length, padding_length, padding_length, cv2.BORDER_CONSTANT)
 
     # Create output image of same size (using np.empty instead of np.zeros to save computational time/power)
     I_output = np.empty(I_input.shape)
-    
+
     # Loop through rows, cols, and each color pixel channel
     for i in range(rows):
         for j in range(cols):
             for c in range(channels):
                 # np.reshape(M, -1) will flatten our matrix to a 1D array
                 # padded_image[i : i + kernel_cols, j : j + kernel_rows, k], slicing our array. [0 : 3, 0 : 3] will go from rows 0 (inclusive) to 3 (exluded)
-                I_output[i][j][c] = np.reshape(H, -1) @ np.reshape(padded_image[i : i + kernel_cols, j : j + kernel_rows, c], -1)
-    
+                I_output[i][j][c] = np.reshape(
+                    H, -1) @ np.reshape(padded_image[i: i + kernel_cols, j: j + kernel_rows, c], -1)
+
     return I_output
 
 #################################################################
@@ -47,6 +48,7 @@ def convolve(I_input, H):
 # Write a function Reduce(I) that takes image I as input and outputs a copy of the image resampled
 # by half the width and height of the input. Remember to Gaussian filter the image before reducing it;
 # use separable 1D Gaussian kernels.
+
 
 def reduce(I_input):
 
@@ -83,28 +85,70 @@ def expand(I_input):
         raise ValueError("Image must be 2D matrix")
     else:
         log.info("Image is exists and is valid")
-    
+
     # Height, width, RGB channels (channels should be 3 - R, G, B)
     rows, cols, channels = I_input.shape
 
     I_upsample = cv2.resize(I_input, (rows * 2, cols * 2))
-    
+
     return I_upsample
 
 #################################################################
 
 # Use the Reduce() function to write the GaussianPyramid(I,n) function, where n is the no. of levels.
 
-def gaussianPyramid(I, n):
-    pass
+
+def gaussianPyramid(I_input, n):
+    # Checking if image is appropriate for manipulation
+    if I_input is None:
+        raise ValueError("Image does not exist")
+    elif I_input.ndim < 2:
+        raise ValueError("Image must be 2D matrix")
+    else:
+        log.info("Image is exists and is valid")
+
+    # Create list to hold each level of pyramid
+    gaussian_pyramind = []
+    gaussian_pyramind.append(I_input)
+
+    # Iterate through each level of pyramid and reduce
+    next_level = I_input
+    for i in range(n):
+        next_level = reduce(next_level)
+        gaussian_pyramind.append(next_level)
+
+    return gaussian_pyramind
 
 #################################################################
 
 # Use the above functions to write LaplacianPyramids(I,n) that produces n level Laplacian pyramid of I.
 
 
-def laplacianPyramid(I, n):
-    pass
+def laplacianPyramid(I_input, n):
+    # Checking if image is appropriate for manipulation
+    if I_input is None:
+        raise ValueError("Image does not exist")
+    elif I_input.ndim < 2:
+        raise ValueError("Image must be 2D matrix")
+    else:
+        log.info("Image is exists and is valid")
+
+    # Construct Gaussian pyramid
+    gaussian_pyramid = gaussianPyramid(I_input, n)
+
+    # Construct Laplacian pyramid
+    laplacian_pyramid = []
+
+    # Bottom to top construction
+    for i in range(n - 1):
+        laplacian_pyramid.append(
+            gaussian_pyramid[i] - expand(gaussian_pyramid[i + 1]))
+
+    # Ln = Gn (Top level)
+    laplacian_pyramid.append(gaussian_pyramid[n - 1])
+
+    return laplacian_pyramid
+
 
 #################################################################
 
