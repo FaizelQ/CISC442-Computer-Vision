5d0b4871c8a00934cc343669b4ca09cfc2c939e7
Faizel Quabili <faizelq@udel.edu>
2023-10-21 23:24:38 -0400

    Q3 Done

--- Homework-7/main.py
+++ Homework-7/main.py
@@ -1,4 +1,5 @@
 import cv2
+import pr1FunctionsWorking as faiCv
 import numpy as np
 
 # Load image in
@@ -19,3 +20,8 @@ next_level = original_image
 for i in range(1, 4):
     next_level = cv2.GaussianBlur(next_level, (5, 5), 0)
     cv2.imwrite(f"./images/Q2-Level-{i}-Multi-scale-Einstein.jpeg", next_level)
+
+# 3) Generate Laplacian planes using a Laplacian kernel of your choice
+laplacian = faiCv.laplacianPyramid(original_image, 4)
+for i in range(len(laplacian)):
+    cv2.imwrite(f"./images/Q3-Laplacian-Level-{i}-Einstein.jpeg", laplacian[i])
