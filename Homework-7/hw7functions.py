d8e14b40058a0789adb9b7451a3461339f9cadd3
Faizel Quabili <faizelq@udel.edu>
2023-10-21 23:59:12 -0400

    Back tracking with functions file instead

---
+++ Homework-7/hw7functions.py
@@ -0,0 +1,57 @@
+import cv2
+'''
+Creates multi-res gaussian pyramid with n levels using cv2.pyrDown() and returns list of images
+'''
+
+
+def multi_res(I_input, n_levels):
+    mrGaussianPyramid = [I_input]
+
+    cv2.imwrite(f"./images/Q1-Level-{0}-Multi-res-Einstein.jpeg", I_input)
+    next_level = I_input
+
+    for i in range(1, 4):
+        next_level = cv2.pyrDown(next_level)
+        mrGaussianPyramid.append(next_level)
+        cv2.imwrite(f"./images/Q1-Level-{i}-Multi-Res-Einstein.jpeg", next_level)
+
+    return mrGaussianPyramid
+
+
+'''
+Creates multi-scale gaussian pyramid with n levels using cv2.GaussianBlur()
+'''
+
+
+def multi_scale(I_input, n_levels):
+    msGaussianPyramid = [I_input]
+
+    cv2.imwrite(f"./images/Q2-Level-{0}-Multi-scale-Einstein.jpeg", I_input)
+    next_level = I_input
+    for i in range(1, 4):
+        next_level = cv2.GaussianBlur(next_level, (5, 5), 0)
+        msGaussianPyramid.append(next_level)
+        cv2.imwrite(f"./images/Q2-Level-{i}-Multi-scale-Einstein.jpeg", next_level)
+
+    return msGaussianPyramid
+
+
+'''
+Creates lapclian pyramid from our multi_scale images
+'''
+
+
+def laplacianPyramid(I_input, n_levels):
+    gaussian_pyramid = multi_scale(I_input, n_levels)
+    laplacian_pyramid = []
+
+    for i in range(0, n_levels - 1):
+        height, width, channels = gaussian_pyramid[i].shape
+        resized = cv2.resize((gaussian_pyramid[i + 1]), (width, height))
+        laplace_level = cv2.subtract(gaussian_pyramid[i], resized)
+        laplacian_pyramid.append(laplace_level)
+        cv2.imwrite(f"./images/Q3-Laplacian_Level-{i}-Einstein.jpeg", laplace_level)
+
+    laplacian_pyramid.append(gaussian_pyramid[n_levels - 1])
+    cv2.imwrite(f"./images/Q3-Laplacian_Level-{n_levels - 1}-Einstein.jpeg", laplacian_pyramid[n_levels - 1])
+    return laplacian_pyramid
--- Homework-7/main.py
+++ Homework-7/main.py
@@ -1,6 +1,7 @@
 import cv2
-import pr1FunctionsWorking as faiCv
+import hw7functions as faiCv
 import numpy as np
+
 
 # Load image in
 
@@ -8,11 +9,7 @@ original_image = cv2.imread("./images/Einstein.jpeg")
 
 # 1) Generate and show four levels of multi-resolution. Use a Gaussian kernel of your choice.
 # Using cv2.pyrDown() to downsample our image, this reduces the size and applies a Gaussian kernel
-cv2.imwrite(f"./images/Q1-Level-{0}-Multi-res-Einstein.jpeg", original_image)
-next_level = original_image
-for i in range(1, 4):
-    next_level = cv2.pyrDown(next_level)
-    cv2.imwrite(f"./images/Q1-Level-{i}-Multi-Res-Einstein.jpeg", next_level)
+mrGaussianPyramid = faiCv.multi_res(original_image, 4)
 
 # 2) Generate and show four levels of multi-scale. Use the same Gaussian kernel as above.
 cv2.imwrite(f"./images/Q2-Level-{0}-Multi-scale-Einstein.jpeg", original_image)
@@ -22,6 +19,5 @@ for i in range(1, 4):
     cv2.imwrite(f"./images/Q2-Level-{i}-Multi-scale-Einstein.jpeg", next_level)
 
 # 3) Generate Laplacian planes using a Laplacian kernel of your choice
-laplacian = faiCv.laplacianPyramid(original_image, 4)
-for i in range(len(laplacian)):
-    cv2.imwrite(f"./images/Q3-Laplacian-Level-{i}-Einstein.jpeg", laplacian[i])
+laplacian_pyramid = faiCv.laplacianPyramid(original_image, 4)
+# 4) Generate an approximation to Laplacian using the difference of Gaussian planes from (1). Note: you need to do 'Expand' on images before taking the difference.
