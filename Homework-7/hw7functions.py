e35adc8756ff3f11834fa65335803e7267719130
Faizel Quabili <faizelq@udel.edu>
2023-10-22 00:05:32 -0400

    Done

--- Homework-7/hw7functions.py
+++ Homework-7/hw7functions.py
@@ -1,4 +1,5 @@
 import cv2
+
 '''
 Creates multi-res gaussian pyramid with n levels using cv2.pyrDown() and returns list of images
 '''
@@ -42,7 +43,7 @@ Creates lapclian pyramid from our multi_scale images
 
 
 def laplacianPyramid(I_input, n_levels):
-    gaussian_pyramid = multi_scale(I_input, n_levels)
+    gaussian_pyramid = multi_res(I_input, n_levels)
     laplacian_pyramid = []
 
     for i in range(0, n_levels - 1):
@@ -54,4 +55,39 @@ def laplacianPyramid(I_input, n_levels):
 
     laplacian_pyramid.append(gaussian_pyramid[n_levels - 1])
     cv2.imwrite(f"./images/Q3-Laplacian_Level-{n_levels - 1}-Einstein.jpeg", laplacian_pyramid[n_levels - 1])
+    return laplacian_pyramid
+
+
+'''
+Function is same as function above, except the cv2.imwrite path names are difference
+'''
+
+
+def q4_laplacianPyramid(I_input, n_levels):
+    gaussian_pyramid = multi_res(I_input, n_levels)
+    laplacian_pyramid = []
+
+    for i in range(0, n_levels - 1):
+        height, width, channels = gaussian_pyramid[i].shape
+        resized = cv2.resize((gaussian_pyramid[i + 1]), (width, height))
+        laplace_level = cv2.subtract(gaussian_pyramid[i], resized)
+        laplacian_pyramid.append(laplace_level)
+        cv2.imwrite(f"./images/Q4-Laplacian_Level-{i}-Einstein.jpeg", laplace_level)
+
+    laplacian_pyramid.append(gaussian_pyramid[n_levels - 1])
+    cv2.imwrite(f"./images/Q4-Laplacian_Level-{n_levels - 1}-Einstein.jpeg", laplacian_pyramid[n_levels - 1])
+    return laplacian_pyramid
+
+
+def q5_laplacianPyramid(I_input, n_levels):
+    gaussian_pyramid = multi_scale(I_input, n_levels)
+    laplacian_pyramid = []
+
+    for i in range(0, n_levels - 1):
+        laplace_level = cv2.subtract(gaussian_pyramid[i], gaussian_pyramid[i + 1])
+        laplacian_pyramid.append(laplace_level)
+        cv2.imwrite(f"./images/Q5-Laplacian_Level-{i}-Einstein.jpeg", laplace_level)
+
+    laplacian_pyramid.append(gaussian_pyramid[n_levels - 1])
+    cv2.imwrite(f"./images/Q5-Laplacian_Level-{n_levels - 1}-Einstein.jpeg", laplacian_pyramid[n_levels - 1])
     return laplacian_pyramid
--- Homework-7/main.py
+++ Homework-7/main.py
@@ -1,7 +1,5 @@
 import cv2
 import hw7functions as faiCv
-import numpy as np
-
 
 # Load image in
 
@@ -21,3 +19,6 @@ for i in range(1, 4):
 # 3) Generate Laplacian planes using a Laplacian kernel of your choice
 laplacian_pyramid = faiCv.laplacianPyramid(original_image, 4)
 # 4) Generate an approximation to Laplacian using the difference of Gaussian planes from (1). Note: you need to do 'Expand' on images before taking the difference.
+q4_laplacian_pyramid = faiCv.q4_laplacianPyramid(original_image, 4)
+# 5) Generate an approximation to Laplacian using the difference of Gaussian planes from (2)
+q5_laplacian_pyramid = faiCv.q5_laplacianPyramid(original_image, 4)
